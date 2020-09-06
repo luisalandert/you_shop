5cb0f602411faaedccbd7be684cb0f0cb80c686a
luisalandert <luludila@gmail.com>
2020-09-05 21:49:05 -0300

    User logs out

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -14,7 +14,21 @@ feature 'User logs in' do
     expect(page).to have_content('Sair')
     expect(page).not_to have_content('Entrar')
   end
+
+  scenario 'and logs out' do
+    user = User.create!(full_name: 'João da Silva', social_name: 'João da Silva',
+                        birth_date: Date.parse('10/10/1990'), job_position: 'Engenheiro de software',
+                        department: 'TI', email: 'joao@email.com', password: '123abc')
+
+    visit root_path 
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Entrar'
+    click_on 'Sair'
+
+    expect(page).to have_content('Entrar')
+    expect(page).not_to have_content('Sair')
+
+  end
 end
-User.create!(full_name: 'Usuário', social_name: 'Usuário',
-  birth_date: Date.parse('10/10/1990'), job_position: 'Engenheiro de software',
-  department: 'TI', email: 'user@email.com', password: '123abc')
