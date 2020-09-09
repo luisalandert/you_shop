e69a1469d688c42d043a5f62283cdce2a219e38a
luisalandert <luludila@gmail.com>
2020-09-09 11:19:59 -0300

    User views own profile

---
+++ spec/features/user_views_own_profile_spec.rb
@@ -0,0 +1,18 @@
+feature 'User views own profile' do
+  scenario 'successfully' do
+    company = Company.create!(name: 'Empresa Dois', cnpj: '08.262.335/7251-10', address: 'Rua Direita, 78',
+                    user_email: 'usuario@empresadois.com.br', email_domain: '@empresadois.com.br')
+    user = User.create!(email: 'joao@empresadois.com.br', password: '123abc', full_name: 'João Pessoa',
+                        social_name: 'João Pessoa', birth_date: Date.parse('10/10/1990'),
+                        job_position: 'Advogado', department: 'Jurídico', status: :complete)
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'João Pessoa'
+    expect(page).to have_content('João Pessoa')
+    expect(page).to have_content('1990-10-10')
+    expect(page).to have_content('Advogado')
+    expect(page).to have_content('Jurídico')
+    # TODO: resolver o problema da data
+  end
+end
