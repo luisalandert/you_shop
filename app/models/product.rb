eec4882739b677bb8f6314f96d12059e985749ea
luisalandert <luludila@gmail.com>
2020-09-10 19:58:52 -0300

    Validation for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_one_attached :picture
+  
+  validates :name, :description, :category, :quantity, :price, :condition,
+            presence: true
 
   enum status: { available: 0, unavailable: 10, suspended: 20 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  context 'validation' do
+    it 'attributes cannot be blank' do
+      product = Product.new
+
+      product.valid?
+
+      expect(product.errors[:name]).to include('não pode ficar em branco')
+      expect(product.errors[:description]).to include('não pode ficar em branco')
+      expect(product.errors[:category]).to include('não pode ficar em branco')
+      expect(product.errors[:quantity]).to include('não pode ficar em branco')
+      expect(product.errors[:price]).to include('não pode ficar em branco')
+      expect(product.errors[:condition]).to include('não pode ficar em branco')
+      expect(product.errors[:user]).to include('é obrigatório(a)')
+    end
+  end
 end
